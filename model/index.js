b2cdadb79f88c41ae61be8a55879ed98de56c1a5
MarinaMuntian <dp060491smv@gmail.com>
2021-11-30 21:16:17 +0200

    update post

--- model/index.js
+++ model/index.js
@@ -1,8 +1,12 @@
 const fs = require('fs/promises')
-const contacts = require('./contacts.json');
+const path = require("path");
+const { v4 } = require("uuid");
+// const contacts = require('./contacts.json');
+
+const contactsPath = path.join(__dirname, './contacts.json');
 
 const listContacts = async () => {
-  const data = await fs.readFile('./model/contacts.json')
+  const data = await fs.readFile(contactsPath)
   const contacts = JSON.parse(data)
   return contacts
 }
@@ -18,7 +22,20 @@ const getContactById = async (contactId) => {
 
 const removeContact = async (contactId) => {}
 
-const addContact = async (body) => {}
+// const addContact = async (body) => { }
+
+const addContact = async(name, email, phone) => {
+  const contacts = await listContacts();
+  const newContact = { name, email, phone, id: v4() };
+
+if (!name || !email || !phone) {
+  return `All fields are required!`
+}
+
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return newContact;
+}
 
 const updateContact = async (contactId, body) => {}
 
